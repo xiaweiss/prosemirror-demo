ef06efaae3f27e5a5a81f02386ce6eacff9085be
Xiawei <xiaweiss@foxmail.com>
2021-07-28 19:03:00 +0800

    wip

--- src/table.js
+++ src/table.js
@@ -16,9 +16,9 @@ const content = createElement(`
 <img src="https://xiawei.cc/images/avatar2.jpg" alt="" />
 <p>The table:</p>
 <table class="table_prosemirror">
-  <tr><td data-colwidth="100">One</td><td>Two</td><td>Three</td><td></td></tr>
-  <tr><td data-colwidth="100">Four</td><td>Five</td><td>Six</td><td></td></tr>
-  <tr><td data-colwidth="100"></td><td></td><td></td></tr>
+  <tr><td data-colwidth="100">One</td><td data-colwidth="100">Two</td><td data-colwidth="100">Three</td><td data-colwidth="100"></td></tr>
+  <tr><td data-colwidth="100">Four</td><td data-colwidth="100">Five</td><td data-colwidth="100">Six</td><td data-colwidth="100"></td></tr>
+  <tr><td data-colwidth="100"></td><td></td data-colwidth="100"><td data-colwidth="100"></td></tr>
 </table>
 
 <p>The table 2:</p>
